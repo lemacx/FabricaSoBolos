23e3eb84c16e7cdc3fb3bbb6203aa361c4062c86
Leticia Maciel <leticiamaciel943@gmail.com>
2025-03-01 18:26:46 -0300

    Adiciona informação a string da classe Bolo

--- BoloSimples.java
+++ BoloSimples.java
@@ -27,6 +27,6 @@ public class BoloSimples extends Bolo {
         } else {
             semLactoseString = "Com lactose";
         }
-        return super.toString() + semLactoseString;
+        return super.toString() + " bolo simples " + semLactoseString;
     }
 }
